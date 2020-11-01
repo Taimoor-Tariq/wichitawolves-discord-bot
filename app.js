94d749efc6a30995f67b50a9f4dd7fd576456e5f
ANN0Y1NGHACKER <45585081+ANN0Y1NGHACKER@users.noreply.github.com>
2020-11-01 01:03:55 -0400

    Release 1.0.0

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ require("./Modules/music");
 app.use(require('./routes'));
 app.use(express.static("public"));
 
-const listener = app.listen(process.env.PORT, () => {
-	console.log("Your app is listening on port " + listener.address().port);
+app.listen(3001, () => {
+	console.log(`Your app is listening on port 3001`);
 });
